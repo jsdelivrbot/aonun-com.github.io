77d3399d45181b70e0d149e92eb25df331d3667b
ddb1494 <ddb1494@gmail.com>
2018-10-09 23:23:48 +0900

    a

--- tm5/__add.nodejs.completions.js
+++
@@ -1,6 +0,0 @@
-const SublimeTextCompletions=require('./SublimeTextCompletions.js');
-
-var stc=new SublimeTextCompletions();
-
-stc.add('SublimeTextCompletions','SublimeTextCompletions');
-stc.save();
--- tm5/pasteOnlyText.js
+++
@@ -1,5 +0,0 @@
-$(window).on('paste',function(e){
-	var o=e.originalEvent;
-	var c=o.clipboardData;
-	c.setData('text/html',c.getData('text/plain'));
-});
