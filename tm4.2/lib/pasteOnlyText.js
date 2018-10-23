3e0d76579c3f9cda7bd5b2fb379b3a386fa06d95
ddb1494 <ddb1494@gmail.com>
2018-10-23 12:45:36 +0900

    a

--- tm4.2/lib/pasteOnlyText.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('paste',function(e){
-	var c=e.clipboardData;
-	c.setData('text/html',c.getData('text/plain'));
-
-	console.log(c.getData('text/plain'));
-	console.log(c.getData('text/html'));
-
-},true);
--- tm4.2/pagecount.php
+++
@@ -1,5 +0,0 @@
-<?php
-$datafile = 'pagecount.data.php';
-@include $datafile;
-if(empty($pagecount)) $pagecount=1;
-file_put_contents($datafile, '<?php $pagecount='.($pagecount+1).';?>');
