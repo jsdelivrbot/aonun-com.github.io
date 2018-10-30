37460d415d3073aa98a29597ad3aaabfb220b343
ddb1494 <ddb1494@gmail.com>
2018-10-30 17:50:23 +0900

    a

--- tm4/dictReplacer.js
+++ tm4/dictReplacer.js
@@ -1,38 +1,86 @@
 let dict = [
+	['\\(a)','*$1'],
 	['ba', 'BO'],
 	['aa', 'AA'],
 	['a', '_'],
-	['', '*'],
-	// undefined,
-	// ' ',
-	[' ', '~'],
-	['_', '~'],
-	['三才', '森次'],
-	['三', '参'],
-	['\\d+', '[$&]'],
+	// ['', '*'],
+	// // undefined,
+	// // ' ',
+	// [' ', '~'],
+	// ['_', '~'],
+	// ['三才', '森次'],
+	// ['三', '参'],
+	// ['[a]', '[$&]'],
 ];
 
-let str = '12a a---- 212 ba aa三苹果明白c aaa三才三';
+let str = '12\\a \\b';
+
+console.log(str);
+console.log(dictReplacer(str, dict, true));
 
 
-function dictReplacer(str, dict) {
-	let regExpArr = dict.filter(e => Array.isArray(e) && (typeof e[0] === 'string' && e[0].length > 0));
-	let regExp = new RegExp(regExpArr.map(e => `(${e[0]})`).join('|'), 'g');
-	let r = str.replace(regExp, function (m) {
-		let v;
+function dictReplacer(str, dict, useRegExp=false) {
+	let regExpArr, regExpStr, regExp, r;
+	regExpArr = dict.filter(e => Array.isArray(e) && (typeof e[0] === 'string' && e[0].length > 0));
+	regExpStr = regExpArr.map(e => {
+		let v = e[0];
+		if(useRegExp) {
+			v = v.replace(/\\/g, '\\\\');
+		}else{
+			v = v.replace(/[\[\]\{\}\^\$\(\)\\\/\?\+\*]/g,'\\$&');
+		}
+		return `(${v})`;
+	}).join('|');
+	regExp = new RegExp(regExpStr, 'g');
+	r = str.replace(regExp, function (...strArgs) {
+		let v, m=strArgs[0];
 		dict.some(e => {
-			let b = e[0] === m;
+			let b = e[0] === strArgs[0];
 			v = e[1];
+			// 支持正则表达式
+			if(useRegExp) {
+				v.replace(/(\$)?\$(\d{1,2}|&)/g, function (...vArgs) {
+					// return vArgs[1] ? vArgs[0].slice(1) : strArgs[vArgs[2]];
+					console.log(JSON.stringify(vArgs));
+					
+					let r;
+					if(vArgs[1]) {
+						r = vArgs[0].slice(1);
+					}else{
+
+						r = strArgs[vArgs[2]];
+						console.log(r);
+						if(r==='&') {
+							r = m;
+						}
+						
+					}
+					return r;
+				});
+			}
 			return b;
 		})
-		console.log(v);
 		return v;
 	});
 	return r;
 }
 	
-	
-// console.log(str);
-console.log(dictReplacer(str, dict));
 
-let r='abcdea'.replace(/a/g, '[$$&]');console.log(r);
+
+
+// let r='abcdea'.replace(/a/g, '[$$&]');console.log(r);
+
+
+// 对$\d进行特别处理的测试
+// {
+// 	let r = '$$1 $$2 $3 $33'.replace(/(\$)?\$\d{1,2}/g, function(...args){
+// 		console.log(args[0], args[1]);
+// 		return args[1] ? args[0].slice(1) : '*';
+// 	});
+// 	console.log(r);
+// }
+
+
+// {
+// 	let s = 
+// }
