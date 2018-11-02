35e3af0735f7fe27458340dec42c9fee7d7019c4
ddb1494 <ddb1494@gmail.com>
2018-11-02 17:55:43 +0900

    longSegmentSplit

--- tm4/test.js
+++ tm4/test.js
@@ -1,6 +1,6 @@
 function longSegmentSplit(str) {
 	// let str = 'aaaa. bbbbbbb.ccccccc.4 4.5%.';
-	let r = /\.+/g;
+	let r = /\!\?+|\?\!+|\?+|\!+|\.+/g;
 	let s = str.split(r);
 	let m = str.match(r)
 	// console.log(JSON.stringify(s));
@@ -15,7 +15,7 @@ function longSegmentSplit(str) {
 		console.log(after);
 		return e + '\0';
 	});
-	p = p.replace(/\{\\r\\n\}|\\r\\n|\\r|\\n/g, '$&\0');
+	p = p.replace(/\{\\r\\n\}|\\r\\n|\\r|\\n/g, '\0$&\0');
 	p = p.split('\0').map(e => e.trim()).filter(e => e.length > 0);
 
 	// console.log(JSON.stringify(p));
@@ -24,4 +24,4 @@ function longSegmentSplit(str) {
 
 
 
-console.log(JSON.stringify(longSegmentSplit('aaa.asdfsa1.\\n2asfasf.asf.')));
+console.log(JSON.stringify(longSegmentSplit('aaa.asdf!sa1..3.\\n2as?fas!?f.a?!sf.')));
