c1b1c133d0588c1caeca3b76ba55ed963655e319
ddb <ddb1494@gmail.com>
2018-11-14 18:00:42 +0900

    a

--- test/module/m1/m.js
+++ test/module/m1/m.js
@@ -1,12 +1,13 @@
 export const name = 'jinxidong';
 export const age = 19831014;
-// 其它文件中导入时，需要名称一致。
-// improt {name} from './m1/m.js'
+// 可以是多个
+// 从其它文件中导入时需要保持名称一致
+// 例： improt {name,age} from './m1/m.js'
 
 export default name;
-// 默认的好处
-// 其它文件中导入时，可以自定义名称。
-// import xxxx from './m1/m.js'
-// xxxx 不可以是 {xxxx}
+// 一个文件中只能有一个
+// 从其它文件中导入时可以自定义名称
+// 名称不用花括号，如：xxxx 即不可以是 {xxxx}
+// 例：import xxxx from './m1/m.js'
 
 console.log('m1-m');
